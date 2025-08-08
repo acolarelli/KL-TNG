4be098450ca91016439fadd10f967de406463800
acolarelli <137216546+acolarelli@users.noreply.github.com>
2025-08-08 16:07:21 -0700

    convolve ePSF

--- pipe.py
+++ pipe.py
@@ -7,6 +7,8 @@ import os
 import astropy.units as u
 from astropy import wcs
 from astropy.io import fits
+import astropy.stats
+from astropy.convolution import convolve
 from pafit import fit_kinematic_pa as fitpa
 import joblib
 import galsim
@@ -27,18 +29,28 @@ import getdist
 
 import requests
 
+from iMANGA_functions import apply_FOV
+
 '''
 PAFit and citation can be found here: https://pypi.org/project/pafit/
+
+ePSF, SDSS filters, and example code for convolution (along with citation instructions) can be found here: https://github.com/lonanni/iMaNGA/blob/main/iMANGA_mockMaNGAdatacubes.ipynb
+
 '''
+
 '''
-To Do:
+To Do: 
 Determine if there is a better way to "flatten" the datacube than just crudely summing over the frequency axis
 A lot of the time shear parameters either hit one prior or both, find out what's causing this
 Find a way/write code to estimate velocity scale radius/Rmax_ST if necessary
 Maybe create a function to make priors narrower for known values like cosi
 Go back to rotator_sample and bugfix more thoroughly
+Maybe change functions to take a "file and save path directory" dict to reduce the number of parameters
 
 '''
+np.set_printoptions(threshold=np.inf)
+
+np.random.seed(42)
 
 #iMaNGA_VAC.fits can be downloaded from here: https://www.tng-project.org/data/docs/specifications/#sec5_4
 hdu_lst = fits.open('/home/acolarelli/path/to/venv/bin/kl_measurement-manga/iMaNGA_vac.fits')
@@ -50,6 +62,8 @@ with open('rotator_indices.txt', 'r') as file:
     rotator_indices = file.readlines()
 rotator_indices = list(map(int, rotator_indices))
 
+
+#VAC Info
 gen_info = hdu_lst[1].data
 image_morphology = hdu_lst[2].data
 spatial_info = hdu_lst[3].data #position of each spaxel, [0] for arcsec and [1] for r_eff
@@ -63,11 +77,24 @@ snr = image_morphology['snr_2Dsersic_fit']
 st_mass = gen_info['TNG_tot_stellar_mass'] #total stellar mass in units of 10^10 solar masses
 zshift = gen_info['obs_redshift']
 
+hdu_psf, headerPSF = fits.getdata("/home/acolarelli/path/to/venv/bin/kl_measurement-manga/ePSF.fits.gz", header=True)
+
+psfg = hdu_psf[:,:, 0]
+psfr = hdu_psf[:,:, 1]
+psfi = hdu_psf[:,:, 2]
+psfz = hdu_psf[:,:, 3]
+
+catalog = fits.open("/home/acolarelli/path/to/venv/bin/kl_measurement-manga/iMaNGA_catalog.fits")
+wave_rest = np.float64(catalog[2].data)
+
+snr_file = np.genfromtxt("/home/acolarelli/path/to/venv/bin/kl_measurement-manga/snr_average.dat")
+snr_avg = snr_file[:,1]
+
 ''''''
 
 def get_FOV_mask(gal):
     imanga_FOV = spatial_info[3,gal,:,:]
-    FOV_mask = np.where(imanga_FOV>-1, True, False)
+    FOV_mask = np.where(imanga_FOV>-1, False, True)
     return FOV_mask #this had a use in an older draft, keeping for now just in case
 
 def get_log10_m_star(gal):
@@ -80,6 +107,11 @@ def get_redshift(gal):
 def get_vmap_data(gal):
     kin = np.ma.masked_invalid(kinematics[0,gal,:,:])
     return kin
+
+def pad_with(vector, pad_width, iaxis, kwargs):
+    pad_value = kwargs.get('padder', 10)
+    vector[:pad_width[0]] = pad_value
+    vector[-pad_width[1]:] = pad_value
 
 ''''''
 
@@ -100,6 +132,118 @@ def get(path, params=None):
         return filename # return the filename string
 
     return r
+
+''''''
+
+def flux_to_photon(waves, image):
+    im = image.copy()
+    for wv in range(len(waves)-1):
+        e_photon = 1.98644568327e-08/waves[wv] #E = hc/lambda, hc in erg*angstrom
+        im[wv,:,:] = image[wv,:,:]/e_photon
+    return im
+
+''''''
+
+#This was borrowed from an iManga notebook, see link above for source and citation instructions
+def convolve_psf(gal_index, datacube):
+    redshift = get_redshift(gal_index)
+    wave = wave_rest*(1+redshift) * 10000 #convert micron to angstrom
+    wave = np.delete(wave,0)
+    grid_ = get_FOV_mask(gal_index)
+    grid, FoV_datacube = apply_FOV(grid_, datacube)
+
+    grid_zero = grid.copy()
+    grid_zero[grid==True] = np.nanmin(FoV_datacube[:, (grid==True)],axis=0)>0
+
+    bkg_x = np.where((grid == True)&(grid_zero==True))[0][-1]
+    bkg_y = np.where((grid == True)&(grid_zero==True))[1][-1]
+    bkg_  = FoV_datacube[:, bkg_x, bkg_y]
+
+    pflux = np.zeros((FoV_datacube.shape))
+    newnoise = np.zeros((FoV_datacube.shape))
+
+    newnoise[:,:,:] = abs(np.sqrt(FoV_datacube[:,:,:])*(np.sqrt(bkg_.reshape(-1,1,1))/snr_avg.reshape(-1,1,1)))
+    pflux[:,:,:] = np.random.normal(FoV_datacube[:,:,:], scale = newnoise[:,:,:])
+    #print(wave)
+    #print(pflux.shape)
+
+    #to avoid unphysical noise produced by the random process 
+    pflux[np.less(pflux, 0., where=~np.isnan(pflux))] = np.min(pflux[np.greater(pflux, 0., where=~np.isnan(pflux))])
+    newnoise[np.less(newnoise, 0., where=~np.isnan(newnoise))] = np.min(newnoise[np.greater(newnoise, 0., where=~np.isnan(newnoise))])
+
+
+    filer_g = np.genfromtxt('/home/acolarelli/path/to/venv/bin/kl_measurement-manga/sdss/SLOAN_SDSS.g.dat')
+    wave_g = filer_g[:,0]
+    response_g = filer_g[:,1]
+
+    filer_r = np.genfromtxt('/home/acolarelli/path/to/venv/bin/kl_measurement-manga/sdss/SLOAN_SDSS.r.dat')
+    wave_r = filer_r[:,0]
+    response_r = filer_r[:,1]
+
+    filer_i = np.genfromtxt('/home/acolarelli/path/to/venv/bin/kl_measurement-manga/sdss/SLOAN_SDSS.i.dat')
+    wave_i = filer_i[:,0]
+    response_i = filer_i[:,1]
+
+    filer_z = np.genfromtxt('/home/acolarelli/path/to/venv/bin/kl_measurement-manga/sdss/SLOAN_SDSS.z.dat')
+    wave_z = filer_z[:,0]
+    response_z = filer_z[:,1]
+    
+    pflux_300 = pflux[:,~np.isnan(pflux[300,:,:]).all((1))]
+    pflux_300 = pflux_300[:,:,~np.isnan(pflux[300,:,:]).all((1))]
+
+    newnoise_300 = newnoise[:,~np.isnan(pflux[300,:,:]).all((1))]
+    newnoise_300 = newnoise_300[:,:,~np.isnan(pflux[300,:,:]).all((1))]
+
+    grid = np.where(grid==0, np.nan,grid)
+    grid_300 = grid[:,~np.isnan(grid).all(1)]
+    grid_300 = grid_300[~np.isnan(grid).all(1)]
+
+    convolve_img = np.zeros((np.shape(pflux_300)))
+    convolve_noise = np.zeros((np.shape(newnoise_300)))
+    print("Convolving PSF... (this may take a few minutes)")
+    for k in range(len(wave)):
+        if wave[k]<=wave_r.min():
+            convolve_img[k, :,:] = np.where(grid_300==True, convolve(pflux_300[k, :,:], psfg), float("Nan"))
+            convolve_noise[k,:,:] = np.where(grid_300==True, convolve(newnoise_300[k, :,:], psfg), float("Nan"))
+
+        if wave_r.min()<wave[k]<=wave_g.max():
+            convolve_img[k, :,:] = np.where(grid_300==True, convolve(pflux_300[k, :,:], np.mean((psfr, psfg), axis=0)), float("Nan"))
+            convolve_noise[k, :,:] = np.where(grid_300==True, convolve(newnoise_300[k, :,:], np.mean((psfr, psfg), axis=0)), float("Nan"))
+
+        if wave_g.max()<wave[k]<=wave_i.min():
+            convolve_img[k, :,:] = np.where(grid_300==True, convolve(pflux_300[k, :,:], psfr), float("Nan"))
+            convolve_noise[k, :,:] = np.where(grid_300==True, convolve(newnoise_300[k, :,:], psfr), float("Nan"))
+
+        if wave_i.min()<wave[k]<=wave_r.max():
+            convolve_img[k, :,:] = np.where(grid_300==True, convolve(pflux_300[k, :,:], np.mean((psfr, psfi), axis=0)), float("Nan"))
+            convolve_noise[k, :,:] = np.where(grid_300==True, convolve(newnoise_300[k, :,:], np.mean((psfr, psfi), axis=0)), float("Nan"))
+
+        if wave_r.max()<wave[k]<=wave_z.min():
+            convolve_img[k, :,:] = np.where(grid_300==True, convolve(pflux_300[k, :,:],psfi), float("Nan"))
+            convolve_noise[k, :,:] = np.where(grid_300==True, convolve(newnoise_300[k, :,:],psfi), float("Nan"))
+
+        if wave_z.min()<wave[k]<=wave_i.max():
+            convolve_img[k, :,:] = np.where(grid_300==True, convolve(pflux_300[k, :,:],np.mean((psfi, psfz), axis=0)), float("Nan"))
+            convolve_noise[k, :,:] = np.where(grid_300==True, convolve(newnoise_300[k, :,:],np.mean((psfi, psfz), axis=0)), float("Nan"))
+
+        if wave_i.max()<wave[k]<=wave_z.max():
+            convolve_img[k, :,:] = np.where(grid_300==True, convolve(pflux_300[k, :,:],psfz), float("Nan"))
+            convolve_noise[k, :,:] = np.where(grid_300==True, convolve(newnoise_300[k, :,:],psfz), float("Nan"))
+
+        if wave[k]>wave_z.max():
+            convolve_img[k, :,:] = np.where(grid_300==True, convolve(pflux_300[k, :,:],psfz), float("Nan"))
+            convolve_noise[k, :,:] = np.where(grid_300==True, convolve(newnoise_300[k, :,:],psfz), float("Nan"))
+
+    print("Converting flux density to photon count...")
+    print("Convolving finished")
+    convolve_img = flux_to_photon(wave, convolve_img)
+    convolve_noise = flux_to_photon(wave, convolve_noise)
+    convolve_flat = np.nansum(convolve_img, axis=0)
+    noise_flat = np.nansum(convolve_noise,axis=0)
+    return noise_flat, convolve_flat    
+
+
+''''''
 
 def get_mock_params(gal):
     cosi = float(np.cos(np.radians(float(inclinations[gal])))) 
@@ -126,8 +270,8 @@ def get_mock_params(gal):
     print("Theta_int (rad): " + str(np.radians(pos_angle)))
     print("i (deg): "+str(inclinations[gal]))
     print("cosi: " + str(cosi))
-    print("r_hl: "+str(r_hl))
-    print("PAFit vsys: "+str(vsys))
+    print("r_hl (arcsec): "+str(r_hl))
+    print("PAFit vsys (km/s): "+str(vsys))
     print("Sersic: "+str(n))
 
 
@@ -144,9 +288,9 @@ def get_mock_params(gal):
     'shared_params-r_hl_disk': r_hl,  # in arcsec
     'shared_params-dx_disk': 0.,  # fraction of r_hl_disk
     'shared_params-dy_disk': 0.,  # fraction of r_hl_disk
-    'shared_params-flux': 4,  # arbitrary units
+    'shared_params-flux': 4e-12,  # arbitrary units
     'shared_params-r_hl_bulge': 0.,  # in arcsec
-    'shared_params-flux_bulge': 1,  # arbitrary units
+    'shared_params-flux_bulge': 1e-12,  # arbitrary units
     'shared_params-dx_bulge': 0.,  # fraction of r_hl_disk
     'shared_params-dy_bulge': 0.,  # fraction of r_hl_disk
     'shared_params-vscale': r_hl,  # in arcsec #not always a great estimate, fix later
@@ -165,11 +309,12 @@ def param_to_dict(mock_params):
 ''''''
 
 def write_data_info(gal_index, run_num, file_path = "/home/acolarelli/", save_path = "/home/acolarelli/test_chain/"):
-    
+
     mock_params = get_mock_params(gal_index)
     #updated_dict = param_to_dict(mock_params)
-    #FOV_mask = get_FOV_mask(gal_index)
-
+    FOV_mask = get_FOV_mask(gal_index)
+    #FOV_mask_img = np.pad(FOV_mask, 75, pad_with, padder=True)
+    #print(FOV_mask_img.shape)
 
     snap_id = gen_info['TNG_snap_id'][gal_index]
     snap = snap_id[:2]
@@ -186,7 +331,7 @@ def write_data_info(gal_index, run_num, file_path = "/home/acolarelli/", save_pa
         print("Existing IFU datacube found")
         data0 = fits.open(file_path+"/"+str(snap)+"_"+str(gal_id)+".fits")[0]
     else:
-        print("IFU datacube not found, pulling from TNG site...") 
+        print("IFU datacube not found, pulling from TNG site... (this may take a few minutes)\nIf there is a crash after downloading, try running the code again.\n") 
         r = get(url)
         data0 = fits.getdata(r)
         print("Datacube downloaded")
@@ -195,8 +340,11 @@ def write_data_info(gal_index, run_num, file_path = "/home/acolarelli/", save_pa
 
     #Sum over the wavelength/frequency axis to "flatten" the datacube and produce a 2D image
     datacube = data0.data
-    flat_data = np.nansum(datacube, axis=0)
-
+    grid, datacube2 = apply_FOV(FOV_mask, datacube)
+    flat_data = np.nansum(datacube2, axis=0)
+    
+    #Do the same, but with the image convolved with the ePSF
+    noise, image = convolve_psf(gal_index, datacube)
 
 
     #Create directory to save data
@@ -206,8 +354,7 @@ def write_data_info(gal_index, run_num, file_path = "/home/acolarelli/", save_pa
     os.makedirs(save_path2, exist_ok=True)
 
 
-    
-
+    #Basic info
     REDSHIFT = get_redshift(gal_index)
     LOG10_MSTAR = get_log10_m_star(gal_index) #float(np.log10(st_mass[gal_index])) + 10.
     LOG10_MSTAR_ERR = 0.0
@@ -215,10 +362,15 @@ def write_data_info(gal_index, run_num, file_path = "/home/acolarelli/", save_pa
 
     #OUTPUT SETTINGS
     IMAGE_SNR = snr[gal_index]
-    IMAGE_SHAPE = flat_data.shape #(150, 150) # (x, y)
-    SKY_VAR_IMAGE = np.ones(IMAGE_SHAPE)*1500
+    IMAGE_SHAPE = image.shape
+
+
+    #Noise from equation 4: https://academic.oup.com/mnras/article/515/1/320/6603844
+    SKY_VAR_IMAGE = noise + 1e-18 #adding a very small value to prevent divide by zero errors, later see if there's a better way to fix this or find a better value for background sky variance
     IMAGE_PIX_SCALE = 0.5 # arcsec/pix
     IMAGE_PSF_FWHM = 2.5 # arcsec
+
+
     '''
     https://arxiv.org/pdf/2203.11575
     iMaNGA "paper I"
@@ -248,17 +400,38 @@ def write_data_info(gal_index, run_num, file_path = "/home/acolarelli/", save_pa
                 'RA': RA_OBJ.value, # not required, but should they be changed anyway?
                 'Dec': DEC_OBJ.value}
 
+
+
     #image_model = ImageModel(meta_image=meta_image)
-    image_data = flat_data #image_model.get_image(updated_dict['shared_params']) #Create galaxy image from input data
-    image_var = image_data + SKY_VAR_IMAGE
-    
+    image_data = image #image_model.get_image(updated_dict['shared_params']) #Create galaxy image from input data
+    image_var =  SKY_VAR_IMAGE #+ image_data
+    #flat_data_mask = np.where(FOV_mask_img==False, image_data, np.nan)
+    #image_var_mask = np.where(FOV_mask_img==False,image_var, np.nan)
+
     # Set variance to match SNR
-    #image_var = Mock._set_snr(image_data, image_var, IMAGE_SNR, 'image', verbose=False) #Is this still needed? Seem to get better results without this
+    image_var = Mock._set_snr(image_data, image_var, IMAGE_SNR, 'image', verbose=True)
+
+    '''
+    plt.clf()
+
+    plt.imshow(flat_data_mask)
+    plt.colorbar(label='Image Data')
+    plt.savefig(f"{save_path2}/image_masked.png")
+
+
+    plt.clf()
+    plt.imshow(image_var_mask)
+    plt.colorbar(label='Image Variance')
+    plt.savefig(f"{save_path2}/img_var_masked.png")
+    image_var = Mock._set_snr(flat_data, image_var, IMAGE_SNR, 'image', verbose=True)
+    #print(image_data)
+    '''
 
     fig = plt.figure(figsize=(12, 6))
 
     ax = fig.add_subplot(121)
     ax.imshow(image_data)
+    fig.colorbar(mappable=ax.imshow(image_data), ax=ax, label="photons/s/m^2")
     ax.set_title('TNG Image Data', fontsize=12)
 
     ax = fig.add_subplot(122, projection=AP_WCS)
@@ -276,9 +449,9 @@ def write_data_info(gal_index, run_num, file_path = "/home/acolarelli/", save_pa
 
     kin = get_vmap_data(gal_index)
 
-    VMAP_SHAPE = (150, 150)  # Dec, RA
+    VMAP_SHAPE = kin.shape  # Dec, RA
     VMAP_PIX_SCALE = 0.5  # arcsec/pix
-    VMAP_SNR = 100 #snr[gal_index]?
+    VMAP_SNR = snr[gal_index] #?
     VMAP_VAR = np.ones(kin.shape)*20 #pranjal says 20km/s, overplot zero shear with current fit to investigate degeneracy structure
 
 
@@ -292,8 +465,12 @@ def write_data_info(gal_index, run_num, file_path = "/home/acolarelli/", save_pa
     vmap_var = VMAP_VAR
 
     # Set variance to match SNR
-    vmap_var = Mock._set_snr(vmap_data, vmap_var, VMAP_SNR, 'image', verbose=False)
+    #vmap_var = Mock._set_snr(vmap_data, vmap_var, VMAP_SNR, 'image', verbose=True) #Is this still needed? Is the velocity map SNR the same as the image SNR?
 
+
+    print(np.nansum(image_data))
+
+    #Plot velocity map
     plt.figure(figsize=(6, 5))
 
     plt.pcolormesh(RA_grid, Dec_grid, vmap_data)
@@ -303,26 +480,29 @@ def write_data_info(gal_index, run_num, file_path = "/home/acolarelli/", save_pa
     plt.colorbar(label='V [km/s]')
     plt.savefig(f'{save_path2}/vel_map.png')
 
-    '''
     plt.clf()
 
-    plt.imshow(kinematics[0,gal_index,:,:])
-    plt.colorbar()
-    plt.savefig(f"{save_path2}/original_vmap.png")
-
-    plt.clf()
-
-    plt.imshow(spatial_info[3,gal_index,:,:])
-    plt.colorbar()
-    plt.savefig(f"{save_path2}/fov_mask.png")
-    '''
+    #Plot image variance
+    plt.imshow(image_var)
+    plt.colorbar(label="Image Variance (photons/s/m^2)")
+    plt.savefig(f"{save_path2}/image_var.png")
 
     plt.clf()
 
+    #Plot non-convolved image (as a sanity check)
+    plt.imshow(flat_data)
+    plt.colorbar(label="Pre-convolution data (photons/s/m^2)")
+    plt.savefig(f"{save_path2}/flat_data.png")
+
+    plt.clf()
+
+    #Plot velocity map variance
     plt.imshow(vmap_var)
     plt.colorbar(label='Vmap variance [km/s]')
     plt.savefig(f"{save_path2}/vmap_var.png")
 
+
+    #Write data info
     data_info = {}
 
     data_info['image'] = {}
@@ -383,6 +563,7 @@ def sampler(gal_index, run_num, save_path = "/home/acolarelli/test_chain/",
 
     plt.rc('figure', dpi=300)
 
+    #Load data info
     data_info = joblib.load(f'{save_path1}/data_info.pkl')
 
     # Re-instantiate the galsim WCS object using the astropy wcs
@@ -393,12 +574,24 @@ def sampler(gal_index, run_num, save_path = "/home/acolarelli/test_chain/",
     with open(config_path, 'r') as stream:
             config = yaml.safe_load(stream)
 
+
     config['TFprior']['log10_vTF'] = np.log10(data_info['mock_params']['shared_params-vcirc'])
     config['TFprior']['sigmaTF_intr'] = 0.05
     config['galaxy_params']['vmap_type'] = 'stellar'
     
     config['galaxy_params']['log10_Mstar'] = data_info['galaxy']['log10_Mstar']
     config['galaxy_params']['log10_Mstar_err'] = 0.
+    
+
+    #Leftover "bandaid fix" from when r_hl was not being properly fitted
+    '''
+    if float(data_info['mock_params']['shared_params-r_hl_disk']) <5.:
+        config['params']['shared_params']['r_hl_disk']['prior']['min'] = 0.1
+    else:
+        config['params']['shared_params']['r_hl_disk']['prior']['min'] = float(data_info['mock_params']['shared_params-r_hl_disk']) - 5.
+    
+    config['params']['shared_params']['r_hl_disk']['prior']['max'] = float(data_info['mock_params']['shared_params-r_hl_disk'])+5.
+    '''
 
     inference = UltranestSampler(data_info=data_info, config=config)
     inference.config
@@ -499,8 +692,8 @@ def sampler(gal_index, run_num, save_path = "/home/acolarelli/test_chain/",
     im3 = ax[1, 2].contourf((image-data)/var, aspect='auto')
 
     divider = make_axes_locatable(ax[1, 1])
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    cbar = fig.colorbar(im2, cax=cax)
+    cax1 = divider.append_axes("right", size="5%", pad=0.05)
+    cbar = fig.colorbar(im2, cax=cax1)
     plt.sca(ax[1, 1])
 
     divider = make_axes_locatable(ax[1, 2])
@@ -516,8 +709,11 @@ def sampler(gal_index, run_num, save_path = "/home/acolarelli/test_chain/",
     with open( f'{save_path2}/config.yaml', 'w') as file:
         yaml.dump(inference.config.__repr__, file)
 
+''''''
+
 def run_pipeline(gal_index, run_num=0, file_path="/home/acolarelli", save_path = "/home/acolarelli/test_chain/", 
                  config_path = '/home/acolarelli/path/to/venv/bin/kl_measurement-manga/config/iMaNGA_config.yaml', preserve_old_runs = False):
+    
     #Preserve old runs doesn't work right now, leave as false and just change run num until fixed
     print("Galaxy#: "+str(gal_index))
     if preserve_old_runs:
@@ -532,10 +728,13 @@ def run_pipeline(gal_index, run_num=0, file_path="/home/acolarelli", save_path =
 
 
 ''''''
-#Note that the file path for the iMaNGA VAC at the start of the code needs to be manually changed for the script to run. 
-#Save paths, datacube file path, and config file path can be changed below.
+
+#Note that the file path for the iMaNGA VAC and other necessary files at the start of the code needs to be manually changed for the script to run. 
+#Save paths and config file path can be changed below.
+
 ''''''
+
 #Change the list index to change which galaxy is being put in, or manually choose a galaxy index from rotator_indices.txt
-run_pipeline(rotator_indices[49], run_num=1)
+run_pipeline(rotator_indices[45], run_num=1)
 file.close()
 hdu_lst.close()
